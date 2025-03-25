cbda9c759bceb7b82a9f5362710ac4faaa3a1da7
Surghe <Milesiii.Irving@gmail.com>
2025-03-24 20:18:44 -0600

    2nd commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,16 @@ import "./styles.css";
 let mainEl = document.createElement('main');
 mainEl.appendChild(mainEl);
 
-let = document.getElementsByTagName('main');
+let mainHeader = document.createElement('main');
+let Header = headers[0]; //i dont know why it didnt assign
+mainHeader.style.backgroundColor = " #4a4e4d";
+
+mainEl.innerHTML = '<h1>DOM Manipulation<h1/>';
+mainEl.classList.add('flex-ctr');
+
+let topMenuEl = document.createElement('<nav id="top-menu">');
+topMenuEl.appendChild(mainEl);
+
+topMenuEl.style.height(100);
+topMenuEl.style.backgroundColor = " #0e9aa7";
+topMenuEl.classList.add('flex-around');
